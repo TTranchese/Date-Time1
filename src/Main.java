1f3a27ed5665e48e844568e814597553d08a456f
TTranchese <tommaso.tranchese.96@gmail.com>
2023-02-23 12:56:29 +0100

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.time.LocalDate;
+import java.time.Year;
 
 
 public class Main {
 	public static void main(String[] args) {
+		//dichiaro e assegno la variabile localDate
 		LocalDate localDate =  LocalDate.now();
+		//dichiaro e assegno la varibile monthDays per sapere da quanti giorni è composto il mese di localDate
 		int monthDays =  localDate.lengthOfMonth();
-		System.out.println("there are "+(monthDays-localDate.getDayOfMonth())+" days left until the end of "+localDate.getMonth());
-		System.out.println("there are "+(localDate.lengthOfYear()-localDate.getDayOfYear())+" days left until the end of "+localDate.getYear());
+		//stampo i giorni mancanti sottraendo il numero dei giorni passati prendendolo da .getDayOfTheMonth in localDale
+		System.out.println("there are "+(monthDays-localDate.getDayOfMonth()));
+		//utilizzo il metodo statico .of della classe Year per farmi tornare un oggetto di tipo year per poi prenderne la lunghezza, sottraggo poi il .getDayOfYear da localDate
+		System.out.println("there are "+(Year.of(localDate.getYear()).length() - localDate.getDayOfYear()));
 	}
 }
